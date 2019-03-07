cd0d6c34d5c602a2ceb3bdeeb3818d2d1ac45c69
initbar <initbar@protonmail.ch>
2019-03-07 10:11:23 -0500

    package inside IIFE

--- webtorrent-backend/webflix.js
+++ webtorrent-backend/webflix.js
@@ -1,15 +1,22 @@
-const WebTorrent = require('webtorrent');
+/**
+ * Webflix
+ */
 
-var client = new WebTorrent()
+const WebTorrent = require('webtorrent')
 
-var torrentId = 'magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+(function() {
+  const client = new WebTorrent()
+  const torrentId = document.getElementById("torrent") || ""
+  if (torrentId) {
+    client.add(torrentId, function(torrent) {
+      var file = torrent.files.find(function(file) {
 
-client.add(torrentId, function (torrent) {
-  // Torrents can contain many files. Let's use the .mp4 file
-  var file = torrent.files.find(function (file) {
-    return file.name.endsWith('.mp4')
-  })
+        // todo: add more media extensions
+        return file.name.endswith(".mp4")
+      })
+    })
+  }
 
-  // Display the file by adding it to the DOM. Supports video, audio, image, etc. files
-  file.appendTo('body')
+  // <div id="wf-app"> or <section id="wf-app">
+  file.appendTo("#wf-app")
 })
