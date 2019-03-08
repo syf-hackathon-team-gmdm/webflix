ed66ca7ab422c1fd881ac4c99317f541e2490e93
initbar <initbar@protonmail.ch>
2019-03-08 01:17:18 -0500

    defer javascript execution until document fully loads

--- source/extension/popup.js
+++ source/extension/popup.js
@@ -1,18 +1,20 @@
-const button = document.getElementById("magnet-link-button");
-const input = document.getElementById("wf-torrent");
+(function() {
 
-button.addEventListener("click", function() {
-  var torrentId = input.value || "";
-  if (!torrentId) {
-    // add shake effect
-    input.classList.add("shake");
-    setTimeout(function() {
-      input.classList.remove("shake");
-    }, 1000);
-  } else {
-    // download torrent
-    webflix.init(torrentId)
-    webflix.download();
-  }
-})
+  const button = document.getElementById("magnet-link-button");
+  const input = document.getElementById("wf-torrent");
 
+  button.addEventListener("click", function() {
+    var torrentId = input.value || "";
+    if (!torrentId) {
+      // add shake effect
+      input.classList.add("shake");
+      setTimeout(function() {
+        input.classList.remove("shake");
+      }, 1000);
+    } else {
+      // download torrent
+      webflix.init(torrentId)
+      webflix.download();
+    }
+  })
+})();
